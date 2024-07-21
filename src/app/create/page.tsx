03fbf7d156b9afb2ce762735dbd45e65382d52ee
crawlin <crawlin@uw.edu>
2024-07-20 20:08:16 -0700

    added date selection from calendar

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -20,8 +20,11 @@ import {
   NumberIncrementStepper,
   NumberDecrementStepper,
 } from '@chakra-ui/react'
-import {Button as CalendarButton, Calendar, CalendarCell, CalendarGrid, DateInput, DatePicker, DateSegment, Dialog, Group, Heading, Label, Popover} from 'react-aria-components';
-import './calendar.css'
+import {Button, Calendar, CalendarCell, CalendarGrid, CalendarGridBody, CalendarGridHeader, CalendarHeaderCell, DateInput, DatePicker, DateSegment, Dialog, Group, Heading, Label, Popover} from 'react-aria-components';
+import type {ButtonProps, PopoverProps} from 'react-aria-components';
+import ChevronUpDownIcon from '@spectrum-icons/workflow/ChevronUpDown';
+import ChevronLeftIcon from '@spectrum-icons/workflow/ChevronLeft';
+import ChevronRightIcon from '@spectrum-icons/workflow/ChevronRight';
 
 
 export default function CreatePage() {
@@ -99,8 +102,98 @@ function Specific() {
   }
 
   return (
-    <Box/>
+    <Box mt={4}>
+      <ChakraButton>Add date or date range</ChakraButton>
+
+      
+      <DatePicker className="group flex flex-col gap-1 w-[200px]">
+        <Label className="text-white cursor-default">
+          Date
+        </Label>
+        <Group className="flex rounded-lg bg-white/90 focus-within:bg-white group-open:bg-white transition pl-3 shadow-md text-gray-700 focus-visible:ring-2 ring-black">
+          <DateInput className="flex flex-1 py-2">
+            {(segment) => (
+              <DateSegment
+                segment={segment}
+                className="px-0.5 tabular-nums outline-none rounded-sm focus:bg-violet-700 focus:text-white caret-transparent placeholder-shown:italic"
+              />
+            )}
+          </DateInput>
+          <Button className="ml-2 mr-2 w-5 h-9 outline-none cursor-default bg-transparent text-gray-600 border-0 rounded-full flex items-center justify-center hover:bg-gray-100 pressed:bg-gray-200 focus-visible:ring ring-violet-600/70 ring-offset-2">
+            <ChevronUpDownIcon size="XS" />
+          </Button>
+        </Group>
+        <MyPopover>
+          <Dialog className="p-6 text-gray-600">
+            <Calendar>
+              <header className="flex items-center gap-1 pb-4 px-1 w-full">
+                <Heading className="flex-1 font-semibold text-2xl ml-2" />
+                <RoundButton slot="previous">
+                  <ChevronLeftIcon />
+                </RoundButton>
+                <RoundButton slot="next">
+                  <ChevronRightIcon />
+                </RoundButton>
+              </header>
+              <CalendarGrid className="border-spacing-1 border-separate">
+                <CalendarGridHeader>
+                  {(day) => (
+                    <CalendarHeaderCell className="text-xs text-gray-500 font-semibold">
+                      {day}
+                    </CalendarHeaderCell>
+                  )}
+                </CalendarGridHeader>
+                <CalendarGridBody>
+                  {(date) => (
+                    <CalendarCell
+                      date={date}
+                      className="w-9 h-9 outline-none cursor-default rounded-full flex items-center justify-center outside-month:text-gray-300 hover:bg-gray-100 pressed:bg-gray-200 selected:bg-violet-700 selected:text-white focus-visible:ring ring-violet-600/70 ring-offset-2"
+                    />
+                  )}
+                </CalendarGridBody>
+              </CalendarGrid>
+            </Calendar>
+          </Dialog>
+        </MyPopover>
+      </DatePicker>
+
+
+
+      {selectedDays.map((day) => (
+        <Day key={day} day={day} />
+      ))}
+    </Box>
   )
+}
+
+function RoundButton(props: ButtonProps) {
+  return (
+    <Button
+      {...props}
+      className="w-9 h-9 outline-none cursor-default bg-transparent text-gray-600 border-0 rounded-full flex items-center justify-center hover:bg-gray-100 pressed:bg-gray-200 focus-visible:ring-2 ring-violet-600/70 ring-offset-2"
+    />
+  );
+}
+
+function MyPopover(props: PopoverProps) {
+  return (
+    <Popover
+      {...props}
+      className={({ isEntering, isExiting }) => `
+        overflow-auto rounded-lg drop-shadow-lg ring-1 ring-black/10 bg-white
+        ${
+        isEntering
+          ? 'animate-in fade-in placement-bottom:slide-in-from-top-1 placement-top:slide-in-from-bottom-1 ease-out duration-200'
+          : ''
+      }
+        ${
+        isExiting
+          ? 'animate-out fade-out placement-bottom:slide-out-to-top-1 placement-top:slide-out-to-bottom-1 ease-in duration-150'
+          : ''
+      }
+      `}
+    />
+  );
 }
 
 interface DateRange {
